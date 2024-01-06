28b3a66f680ef5bfea66e11a9130c6a45a59d0a1
Андрій <rukom@ukr.net>
2024-01-06 21:14:26 +0200

    Update Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,13 +1,10 @@
-import { Component } from "react";
 import css from './Section.module.css';
 
-export class Section extends Component {
-  render() {
-    return (
-      <>
-        <h2 className={css.title}>{this.props.title}</h2>
-        <div>{this.props.children}</div>
-      </>
-    )
-  }
-}
+export const Section = ({ title, children }) => {
+  return (
+    <>
+      <h2 className={css.title}>{title}</h2>
+      <div>{children}</div>
+    </>
+  );
+};
